c77a2adf66d6154b0b1e52913a37ad2bbd8bf6ac
bobrowadam <adam@bigpanda.io>
2019-10-22 01:06:40 +0300

    Change Main

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,8 +1,3 @@
 package fp
 
-import fp.chapters.three.{ monoids }
-
-object Hello extends App {
-  import monoids.intAddition.op
-  println(op(op(1,2), 3) == op(1,op(2, 3)) )
-}
+object Fp extends App {}
